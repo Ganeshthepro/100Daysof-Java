9af401af6c0f813426ed00a62f2873cc2294a5d8
Ganeshthepro <ganeshnawale29003@gmail.com>
2024-01-09 23:58:15 +0530

    Day3

---
+++ Day3/pattern1.java
@@ -0,0 +1,18 @@
+package Day3;
+import java.util.*;
+
+public class pattern1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        int st = 1;
+        int sp = n-1;
+
+        int number_of_line = 1;
+
+        while (number_of_line<=n~) {
+            
+        }
+    }
+}
