f93f11fe6be0f6427e00f1c3a19468b942237f52
Ganeshthepro <ganeshnawale29003@gmail.com>
2024-02-07 00:20:12 +0530

    Day5

--- Day5/sumofint.java
+++ Day5/sumofint.java
@@ -2,16 +2,26 @@ package Day5;
 import java.util.*;;
 
 public class sumofint {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int num = sc.nextInt();
-        int sum = 0;
 
+    public static int getSum(int num){
+        int sum=0;
         while (num!=0) {
             sum+=num%10;
             num/=10;
         }
-        System.err.println(sum);
+        return sum;
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+        // int sum = 0;
+
+        // while (num!=0) {
+        //     sum+=num%10;
+        //     num/=10;
+        // }
+        // System.err.println(sum);
+        System.out.println(getSum(num));
     }
     
 }
