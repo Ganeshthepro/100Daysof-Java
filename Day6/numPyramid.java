47cb3bfede6e6e249df8fd61590ee2cc1b12a419
Ganeshthepro <ganeshnawale29003@gmail.com>
2024-02-10 13:32:08 +0530

    Day6

--- Day6/numPyramid.java
+++ Day6/numPyramid.java
@@ -1,5 +1,24 @@
 package Day6;
 
+import java.util.Scanner;
+
 public class numPyramid {
-    
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+
+        for(int i=1;i<=num;i++){
+            for(int j = 0; j<num-i;j++){
+                System.out.print(' ');
+            }
+            for(int j=0; j<2*i-1; j++){
+                if (j%2==0) {
+                    System.out.print(i);
+                } else{
+                    System.out.print(' ');
+                }
+            }
+            System.out.println();
+        }
+    }
 }
