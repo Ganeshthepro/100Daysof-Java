534bc403b5d0fb0a07a9ba3cb9c3bc3cedda9cab
Ganeshthepro <ganeshnawale29003@gmail.com>
2024-02-18 12:39:46 +0530

    Day8

--- Day8/array.java
+++ Day8/array.java
@@ -1,7 +1,12 @@
 package Day8;
 
+
 public class array {
     public static void main(String args[]){
         int [] rollno = new int[5] ;
+        System.out.println(rollno[4]);
+
+        String arr[] = new String[3];
+        System.out.println(arr[1]); 
     }
 }
---
+++ Day8/arrinput.java
@@ -0,0 +1,18 @@
+package Day8;
+
+import java.util.*;
+
+public class arrinput {
+    public static void main(String[] args){
+    Scanner sc =  new Scanner(System.in);
+    int[] arr = new int[5];
+
+    for(int i=0;i< arr.length; i++){
+        arr[i] = sc.nextInt();
+    }
+
+    for( int j = 0; j <arr.length;j++){
+        System.out.print(arr[j]+" ");
+    }
+}
+}
